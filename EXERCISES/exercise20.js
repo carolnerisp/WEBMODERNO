94a24cabbdb78ebdae8d7608b25e7453f6e792a1
carolnerisp <carol.nerispereira@gmail.com>
2022-04-19 17:25:33 -0300

    alteração no exercício 20

--- EXERCISES/exercise20.js
+++ EXERCISES/exercise20.js
@@ -4,45 +4,23 @@ Seu programa deve mostrar apenas as notas utilizadas. Por exemplo, ao solicitar 
 informar apenas a seguinte informação (note que não foram exibidas informações sobre as demais cédulas): 1
 nota(s) de R$ 10. 1 nota(s) de R$ 5. 3 nota(s) de R$ 1. */
 
-function calculaCedulas(x){
-  const valor = isNaN(x) ? 0 : x;
-  let notas100, notas50, notas10, notas5, notas1, resto = valor, retorno = '';
+function calculaCedulas(valor) {
+  let valorInformado = isNaN(valor) ? 0 : valor;
+  let quantidadeNotas = 0, retorno = '', cedulaAtual = 0;
+  const cedulas = [100, 50, 10, 5, 1];
 
-  if (valor >= 100) {
-    notas100 = Math.trunc(valor / 100);
-    resto = valor % 100;
+  resto = valorInformado;
 
-    retorno = notas100.toString() + ' nota(s) de R$100. ';
-  }  
-  
-  if (resto >= 50) {
-    notas50 = Math.trunc(resto / 50);
-    resto = valor % 50;
+  for (cedula in cedulas){
+    cedulaAtual = cedulas[cedula]
 
-    retorno = retorno + notas50.toString() + ' nota(s) de R$50. ';
+    if (resto >= cedulaAtual){
+      quantidadeNotas = Math.trunc(resto / cedulaAtual ); 
+      resto = valorInformado % cedulaAtual;
+
+      retorno = retorno + quantidadeNotas.toString() + ` nota(s) de R$${cedulaAtual}. `;
+    }
   }
-
-  if (resto >= 10) {
-    notas10 = Math.trunc(resto / 10);
-    resto = valor % 10;
-
-    retorno = retorno + notas10.toString() + ' nota(s) de R$10. ';
-  }
-
-  if (resto >= 5) {
-    notas5 = Math.trunc(resto / 5);
-    resto = valor % 5;
-
-    retorno = retorno + notas5.toString() + ' nota(s) de R$5. ';
-  }
-
-  if (resto >= 1) {
-    notas1 = Math.trunc(resto / 1);
-    resto = valor % 5;
-
-    retorno = retorno + notas1.toString() + ' nota(s) de R$1. ';
-  }
-  
   return retorno;
 }
 
---
+++ EXERCISES/exercise27.js
@@ -0,0 +1,11 @@
+/**Construa uma função que receba como parâmetros as alturas e as taxas de crescimento anuais de duas
+crianças e calcule se existe uma criança menor, caso exista se a criança menor ultrapassará a maior e em
+quantos anos isso acontecerá. Utilize centímetros para as unidades de medida. */
+
+function calculaAltura (alturaEmCm, taxaDeCrescimentoAnual){
+  const alturaAtual = isNaN(alturaEmCm) ? 0 : alturaEmCm;
+  const taxaDeCrescimento = isNaN(taxaDeCrescimentoAnual) ? 0 : taxaDeCrescimentoAnual;
+  
+  
+
+}
